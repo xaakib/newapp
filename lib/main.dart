b36d57dadbc3b5e1544102a362ce9f960811f6a5
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-03 00:03:38 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/homepage.dart';
+
+void main() {
+  MyApp();
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
